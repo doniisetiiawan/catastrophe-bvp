2d5f196924e649005ad09328cd3e1da424d38631
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-17 12:15:29 +0700

    Our second component

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginContainer from './loginContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div id="container">
+      <LoginContainer />
+    </div>
+  );
+}
+
+export default App;
