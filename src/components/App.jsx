d7b5f18b8ef8c22a2bec67d8d38a7610ae4bf19c
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-17 13:31:16 +0700

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import LoginContainer from './loginContainer';
 import './App.css';
+import Firebase from 'firebase';
 
-function App() {
-  return (
-    <div id="container">
-      <LoginContainer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null,
+    };
+  }
+
+  componentDidMount = () => {
+    Firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div id="container">
+        <LoginContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
